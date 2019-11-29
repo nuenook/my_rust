22f3f517679d0fb12c5c12990c4facfb95ab546a
nuenook <nook.ittipon@gmail.com>
2019-11-29 17:54:38 +0700

    traits read/write file and read user input

--- traits_and_io/src/main.rs
+++ traits_and_io/src/main.rs
@@ -1,11 +1,42 @@
 
-// reading file
+// reading file // write file
 use std::fs::File;
 use std::io::prelude::*;
+
+
+// cli
+use std::env;
+
+// reading user input
+use std::io;
 
 struct Person {
     name: String,
     age: u8
+}
+
+// Defining Traits
+trait HasVoiceBox {
+    // Speak
+    // Check if can Speak
+
+    fn speak(&self);
+
+    fn can_speak(&self) -> bool;
+}
+
+// Defining Traits
+impl HasVoiceBox for Person {
+    fn speak(&self) {
+        println!("Hello my name is {}", self.name);
+    }
+
+    fn can_speak(&self) -> bool {
+        if self.age > 0 {
+            return true;
+        }
+        return false;
+    }
 }
 
 // implement Traits ?
@@ -39,4 +70,64 @@ fn main() {
     file.read_to_string(&mut contents).expect("Oops! Can not read the file...");
 
     println!("File Contents: \n\n{}", contents);
+
+
+    // cli argrument
+    // ran with cargo run arg1 arg2
+    let args: Vec<String> = env::args().collect();
+
+    for argument in args.iter() {
+        println!("{}", argument);
+    }
+
+    // writing file
+    let mut file = File::create("output.txt")
+        .expect("Could not create file");
+    
+    file.write_all(b"Welcome to dcode!").expect("cannot write to the file, sorry mate");
+
+
+    // Defining Traits
+    let person = Person {
+        name: String::from("Nook"),
+        age: 0
+    };
+    
+    println!("Can {} speak? {}", person.name, person.can_speak());
+
+    person.speak();
+
+
+    // pattern matching
+    let match_number = 12;
+
+    match match_number {
+        1 => println!("It is ONe"),
+        2 => println!("There is two of them"),
+        10 | 11 => println!("it is either 10 or 11"),
+        12...15 => println!("it is greater than 12 and less than 15"),
+        _ => println!("IT doesn't match")
+    };
+
+    let match_string = "Nook";
+
+    match match_string {
+        "Nook" => println!("Nice Name, nook"),
+        _ => println!("nono name")
+    }
+
+
+    // Reading User Input;
+    let mut input = String::new();
+
+    println!("Hey mate! say something");
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("Success! you said: {}", input);
+        },
+        Err(e) => {
+            println!("Opp! SOmthing went wrong {}", e);
+        }
+    }
 }

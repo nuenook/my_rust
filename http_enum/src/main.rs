ba84cfe91165db1deabe3794a272541e0a9a68b4
nuenook <nook.ittipon@gmail.com>
2019-11-30 16:26:20 +0700

    http request and enum impl

---
+++ http_enum/src/main.rs
@@ -0,0 +1,72 @@
+#![allow(dead_code)]
+
+extern crate reqwest;
+// enum method
+enum Day{
+    Monday, Tuesday, Wednesday,
+    Thursday, Friday, Saturday, Sunday
+}
+
+impl Day {
+    fn is_weekday(&self) -> bool {
+        match self {
+            &Day::Saturday | &Day::Sunday => return false,
+            _ => return true
+        }
+    }   
+}
+
+fn main() {
+
+    // Option enum
+    let name = String::from("Domenic");
+    println!("charater at index 8: {}", match name.chars().nth(8) {
+        Some(c) => c.to_string(),
+        None => "No character at index 8!".to_string()
+    });
+
+    println!("Occupation is {}", match get_occupation("Domenic") {
+        Some (o) => o,
+        None => "No occupation"
+    });
+
+
+    //Http
+    match reqwest::get("https://jsonplaceholder.typicode.com/todos/1") {
+        Ok(mut response) => {
+            // check if 200 ok
+            if response.status() == reqwest::StatusCode::Ok {
+                match response.text() {
+                    Ok(text) => println!("Response text: {}", text),
+                    Err(_) => println!("Could not read response text")
+                }
+            } else {
+                println!("Response was not 200 OK.")
+            }
+        },
+        Err(_) => println!("Cloud not make the request!")
+    };
+
+    let response_text = reqwest
+        ::get("https://jsonplaceholder.typicode.com/todos/1")
+        .expect("Could not make request")
+            .text().expect("Could not read response text");
+
+    println!("response_text is {}", response_text);
+
+
+    //Enum method
+    let dday = Day::Tuesday;
+    let d2 = Day::Saturday;
+    println!("Is d a weekday? {}", dday.is_weekday());
+    println!("Is d2 a weekday? {}", d2.is_weekday());
+}
+
+// Option Enum
+fn get_occupation(name: &str) -> Option<&str> {
+    match name {
+        "Domenic" => Some("Software Developer"),
+        "Michael" => Some("Dentist"),
+        _ => None
+    }
+}
